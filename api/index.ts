b1a03a72e7fca9e85001aa29358798cb288f67b4
Artem <panasuck@bigmir.net>
2024-05-19 13:54:49 +0300

    added start function

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
-//mongodb+srv://ArtRIFF:<password>@cluster0.psgko3h.mongodb.net/
-app.get("/", (req: any, res: { send: (arg0: string) => any; }) => res.send("Express on Vercel"));
+
+
+app.get("/", (req: any, res: { send: (arg0: string) => any; }) => {
+	console.log("/----------------");
+  res.send("Express on Vercel")
+	
+});
+
+app.get("/check", (req: any, res: { send: (arg0: string) => any; }) => {
+  console.log("/check----------");
+	res.send("checked")
+	
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+
+
