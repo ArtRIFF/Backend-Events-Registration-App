d6c1b1780b9924eb8814449147bf78d851ff36c1
Artem <panasuck@bigmir.net>
2024-05-20 16:49:27 +0300

    Added server comunication

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
+import { ObjectId } from "mongodb";
+import { TEventsCard, TParticipan } from "./types";
+
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
@@ -12,42 +15,132 @@ const databaseName = process.env.MONGO_DBNAME || "";
 
 const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/${databaseName}?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
-  console.log("/----------------");
-  res.send("Express on Vercel");
-});
+async function getCardsByPage<T>(
+  skip: number,
+  collectionName: string,
+  searchCriteria: {} = {}
+): Promise<{
+  totalCardCount: number;
+  cards: Array<T>;
+}> {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const database = client.db(databaseName);
+    const collection = database.collection(collectionName);
+
+    const cards = await collection
+      .find(searchCriteria)
+      .skip(skip)
+      .limit(5)
+      .toArray();
+    const totalCardCount = await collection.countDocuments(searchCriteria);
+
+    return { totalCardCount, cards };
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  } finally {
+    await client.close();
+  }
+}
 
 app.get(
-  "/check",
+  "/",
   async (
     req: any,
     res: {
+      json: any;
+      status: any;
+      send: (arg0: string) => void;
+    }
+  ) => {
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const skip = (page - 1) * 4;
+      const { totalCardCount, cards } = await getCardsByPage<TEventsCard>(
+        skip,
+        "events"
+      );
+      res.json({ totalCardCount, cards });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+app.get(
+  "/participans",
+  async (
+    req: any,
+    res: {
+      json(arg0: { totalCardCount: number; cards: TParticipan[] }): unknown;
       send: (arg0: any) => void;
       status: (arg0: number) => {
+        json: any;
         (): any;
         new (): any;
         send: { (arg0: string): void; new (): any };
       };
     }
   ) => {
-    console.log("/check----------");
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const eventId = req.query.eventId;
+
+      const skip = (page - 1) * 4;
+      const searchCriteria = {
+        event_id: new ObjectId(eventId),
+      };
+
+      const { totalCardCount, cards } = await getCardsByPage<TParticipan>(
+        skip,
+        "participans",
+        searchCriteria
+      );
+      res.json({ totalCardCount, cards });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+app.post(
+  "/registration",
+  async (
+    req: { body: any },
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { id?: any; message?: string }): void; new (): any };
+      };
+    }
+  ) => {
+    const participant = req.body;
+
+    const client = new MongoClient(uri);
 
     try {
-      const client = new MongoClient(uri);
       await client.connect();
+      console.log("Connected to MongoDB");
 
       const database = client.db(databaseName);
-      const collection = database.collection("users");
+      const collection = database.collection("participans");
 
-      const users = await collection.find({}).limit(4).toArray();
+      const result = await collection.insertOne(participant);
+      console.log(`Participant added with ID: ${result._id}`);
 
-      res.send(users);
-      console.log("users");
-      console.log(users);
-      await client.close();
+      res.status(201).json({ id: result.insertedId });
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
-      res.status(500).send("Error connecting to MongoDB");
+      res.status(500).json({ message: "Internal server error" });
+    } finally {
+      await client.close();
     }
   }
 );
